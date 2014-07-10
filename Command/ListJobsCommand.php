e6923e738116a05dc8d0493d794f0c8c2b4c1e98
Gildas Quéméner <gildas.quemener@gmail.com>
2014-07-10 12:06:51 +0200

    Fixed header

--- Command/ListJobsCommand.php
+++ Command/ListJobsCommand.php
@@ -10,10 +10,10 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 /**
  * Lists active batch jobs
- * 
+ *
  * @author    Antoine Guigan <antoine@akeneo.com>
  * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
- * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @license   http://opensource.org/licenses/MIT MIT
  */
 class ListJobsCommand extends ContainerAwareCommand
 {
