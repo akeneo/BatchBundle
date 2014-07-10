467e5697e9b8c546f486866fb0064ed1ef049fb8
Gildas Quéméner <gildas.quemener@gmail.com>
2014-07-10 12:06:08 +0200

    Written push stdout handler test

---
+++ Tests/Unit/EventListener/PushStdOutLoggerHandlerListenerTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Akeneo\Bundle\BatchBundle\Tests\Unit\EventListener;
+
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\NullOutput;
+use Symfony\Component\Console\Event\ConsoleCommandEvent;
+use Akeneo\Bundle\BatchBundle\EventListener\PushStdOutLoggerHandlerListener;
+use Akeneo\Bundle\BatchBundle\Command\BatchCommand;
+
+/**
+ * Test related class
+ */
+class PushStdOutLoggerHandlerListenerTest extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        $this->logger = $this->getMockBuilder('Monolog\Logger')->disableOriginalConstructor()->getMock();
+        $this->subscriber = new PushStdOutLoggerHandlerListener($this->logger);
+    }
+
+    public function testPush()
+    {
+        $command = new BatchCommand();
+        $input = new ArrayInput([]);
+        $output = new NullOutput();
+        $event = new ConsoleCommandEvent($command, $input, $output);
+
+        $this
+            ->logger
+            ->expects($this->once())
+            ->method('pushHandler')
+            ->with($this->callback(function ($subject) {
+                return $subject instanceof \Monolog\Handler\StreamHandler;
+            }));
+
+        $this->subscriber->push($event);
+    }
+}
